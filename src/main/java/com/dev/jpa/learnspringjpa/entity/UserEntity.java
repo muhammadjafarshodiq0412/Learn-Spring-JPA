6e19c2a1ae56024c802e5b26096403a4c14e382f
Jafar <mjafar.shodiq@myboost.co>
2023-03-10 07:51:25 +0700

    add property user entity

--- src/main/java/com/dev/jpa/learnspringjpa/entity/UserEntity.java
+++ src/main/java/com/dev/jpa/learnspringjpa/entity/UserEntity.java
@@ -3,9 +3,17 @@ package com.dev.jpa.learnspringjpa.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.util.Date;
+
 @Entity(name = "USER")
 public class UserEntity {
     @Id
     private Long id;
     private String username;
+    private Boolean isActived;
+    private Long role;
+    private Date createdDate;
+    private String createdBy;
+    private Date updatedDate;
+    private String updatedBy;
 }
